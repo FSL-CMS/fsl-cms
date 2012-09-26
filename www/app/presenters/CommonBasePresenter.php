167bbc2efff5e65560b8b2b10e459b5d4837c241
Milan Pála <milan.pala@email.cz>
2012-09-26 19:28:34 +0200

    Vydána verze 1.0.3-rc1.

--- www/app/presenters/CommonBasePresenter.php
+++ www/app/presenters/CommonBasePresenter.php
@@ -36,7 +36,7 @@ abstract class CommonBasePresenter extends Presenter
 	/**
 	 * Verze FSL CMS
 	 */
-	const FSL_CMS_VERZE = '1.0.3-dev';
+	const FSL_CMS_VERZE = '1.0.3-rc1';
 
 	/**
 	 * Odkaz na hlavní stránku FSL CMS
